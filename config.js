28bc8a5b3c891ac546b4ffc7646cf9e5892e56df
Sugger <87353568+Sugger25e@users.noreply.github.com>
2023-06-16 08:29:37 +0800

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  token: "OTY1MzE0Nzc0MTA5OTI1NDI2.G_tx-C.pfB0keF8e-L-DUwRX9XQQCHMzsNqxuwbyukbUA",
-  global: false,
-  mongo: "mongodb+srv://Sugger:sugger25mongoose@shoger.cpkezhk.mongodb.net/test",
-  guild: "786023191663345684",
-}
